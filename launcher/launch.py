4df1cb6e76f5baec8128e9c83e57e5e3ca3841d2
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-10-04 19:10:37 +0300

    Add graceful shutdown

--- launcher/launch.py
+++ launcher/launch.py
@@ -3,6 +3,7 @@ interact with it RESTfully."""
 import os
 import pty
 from select import POLLIN, poll
+import time
 
 from websocket_server import WebsocketServer
 
@@ -72,6 +73,18 @@ class Launcher:
                     self.send_message(out.decode())
         except OSError:
             self.done = True
+        self.graceful_shutdown()
+
+    def graceful_shutdown(self, time_to_wait: int = 10) -> None:
+        """Gracefully shutdown the server.
+
+        Args:
+            time_to_wait (int): Time to wait before shutting down. Defaults to 10.
+        """
+        while time_to_wait > 0:
+            self.send_message(f"\rShutting down in {time_to_wait} seconds.")
+            time.sleep(1)
+            time_to_wait -= 1
 
 
 def main():
